11c64745173dc8451d3ff76653ff9475a034ba25
kengo922 <kengo.matsuno.922@gmail.com>
2020-06-03 14:01:10 +0900

    v4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'profiles#index'
-  resources :profiles, only: [:index, :show]
+  root to: "products#index"
+  resources :products do 
+    collection do
+      get 'mypage' #マイページ
+      get 'profile' #プロフィール       
+    end
+  end
 end
